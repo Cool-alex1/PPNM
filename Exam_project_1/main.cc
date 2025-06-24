ece74339bedbaa6173672dbbc86dbcb8b264048b
Alexander <alexander.paasch.g@gmail.com>
2025-06-24 13:07:38 +0200

    Exam

--- Exam_project_1/main.cc
+++ Exam_project_1/main.cc
@@ -47,7 +47,7 @@ vector newton(const std::function<double(vector)>& f, vector v, matrix H, double
 //     v = [0.777993, 0.628274]
     
     while (n<30) {
-        // std::cout << "R = " << R_val << std::endl;
+        std::cout << "R = " << R_val << std::endl;
         // H.print("H = ");
 
         /*  Gradient check  */
@@ -69,38 +69,49 @@ vector newton(const std::function<double(vector)>& f, vector v, matrix H, double
         Δvm = toMatrix(Δv).T();
     
         std::function<double(double)> f_α = [&](double α){
-            return (vm.T()*H*vm - 2*α*Δvm.T()*H*Δvm)(0,0) / (v*v + α*α*Δv*Δv);
+            return (vm.T()*H*vm - 2*α*Δvm.T()*H*vm + α*α*Δvm.T()*H*Δvm)(0,0) / (v*v + α*α*Δv*Δv);
         };
 
         //My own local minimum finder:
         double a = 0.0;    //Startguess for α
-        double da = 0.1;   //Steplength for α
+        double da = +0.1;   //Steplength for α
         double fα_val;
         double fα_val2;
         while (1){
+            // fα_val = f_α(a);
+            // fα_val2 = f_α(a + da);
             fα_val = f_α(a);
             fα_val2 = f_α(a + da);
 
-            if (std::abs(fα_val - fα_val2) < 1e-4) break; //If change is to small a good α has been found
+
+            if (std::abs(fα_val - fα_val2) < 1e-8) break; //If change is to small a good α has been found
 
             if((fα_val2 - fα_val) > 0){   //If the gradient between f(α) and the next value ever becomes positive (uphill)
                 da /= -2;                 //Turn around and take smaller steps
             }
-            
             a += da;
         }
-        // std::cout << "α = " << a << std::endl;
-        (a*Δv).print("adv = ");
+        std::cout << "α = " << a << std::endl;
+        if(n==29){
+            std::cout << "\n" << std::endl;
+            
+            for(double i=-200.0; i<200; i++){
+                std::cout << i/100 << " " << f_α(i/100) << " " << f(v-(i/100)*Δv) << " " << a << std::endl;
+            }
+            std::cout << "\n" << std::endl;
+        }
+        // (a*Δv).print("adv = ");
         // std::cout << "\n" << std::endl;
 
         //Update v
+        R_val = f(v-a*Δv);
         v -= a*Δv;
-        v /= v.norm();
+        // v /= v.norm();
         R_val = f(v);
         // v.print("v = ");
         n += 1;
     }
-    v /= v.norm();
+    // v /= v.norm();
     return v;
 }
 
@@ -126,7 +137,7 @@ int main(int argc, char** argv) {
     // std::cout << "\n" << std::endl;
     
     // vector col = toVector(H.getCol(1))+3;
-    vector col = vector({3, 0});
+    vector col = vector({1, 1});
     // col.print("c = ");
     
     std::function<double(vector)> R = [=](vector v){
@@ -134,12 +145,12 @@ int main(int argc, char** argv) {
         return (vm * H * vm.T())(0,0) / (v*v);  //Since toMatrix([1,1]) --> [[1,1]], vm and vm.T are swapped
     };                                          //matrix * matrix --> matrix, therefore the only element must be exracted, hence "(0,0)"
     
-    col.print("v_col = ");
+    // col.print("v_col = ");
     vector n = newton(R, col, H, 1e-5);
-    double R_val = R(col);
-    std::cout << R_val << "\n" << std::endl;
-    std::cout << R(n) << "\n" << std::endl;
-    n.print("v_opt1 = ");
+    // double R_val = R(col);
+    // std::cout << R_val << "\n" << std::endl;
+    // std::cout << R(n) << "\n" << std::endl;
+    // n.print("v_opt1 = ");
     // double R_opt1 = R(n);
     // n /= n.norm();
     // n.print("v_opt2 = ");
@@ -148,7 +159,7 @@ int main(int argc, char** argv) {
     // std::cout << R_opt2 << std::endl;
     
     
-    (H/R(n) * toMatrix(n).T()).print("v? = ");
+    // (H/R(n) * toMatrix(n).T()).print("v? = ");
     // (H/6.30283 * toMatrix(vector({0.778518, 0.627622})).T()).print("v? = ");
     // (toVector(H * toMatrix(vector({0.777993, 0.628274})).T())/6.30506).print("v? = ");
 
